0905e8c8e0151a009db960ef4754b249b0b7372d
Onur Akalın <onurakalin96@gmail.com>
2025-04-23 20:02:47 +0300

    validasyonlar eklendi

--- src/main/java/com/example/demo/dtos/requests/CreateBrandRequest.java
+++ src/main/java/com/example/demo/dtos/requests/CreateBrandRequest.java
@@ -1,8 +1,12 @@
 package com.example.demo.dtos.requests;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateBrandRequest {
+    @Size(min = 2, max = 20, message = "Brand name must be between 2 and 20 characters")
+    @NotBlank(message = "Brand name cannot be empty")
     private String name;
 }
--- src/main/java/com/example/demo/dtos/responses/GetModelResponse.java
+++ src/main/java/com/example/demo/dtos/responses/GetModelResponse.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class GetModelResponse {
     private Long id;
     private String name;
+    private Long brandId;
 }
