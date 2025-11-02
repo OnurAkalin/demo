a76bd0b0801f547eaad12c6ade18797c3f404bbc
Onur Akalın <onurakalin96@gmail.com>
2025-11-03 01:08:46 +0300

    .

--- src/test/java/com/example/demo/services/BrandServiceImplTest.java
+++ src/test/java/com/example/demo/services/BrandServiceImplTest.java
@@ -16,6 +16,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.util.Collections;
 import java.util.List;
@@ -26,6 +28,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class BrandServiceImplTest {
 
     @Mock
