a886ae36458cd290fcdb8df9a16e4fd8a8b91a4d
Onur Akalın <onurakalin96@gmail.com>
2026-02-03 22:03:03 +0300

    .

--- src/main/java/com/example/demo/config/RedisCacheConfig.java
+++ src/main/java/com/example/demo/config/RedisCacheConfig.java
@@ -3,7 +3,6 @@ package com.example.demo.config;
 import com.example.demo.constants.CacheConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -17,7 +16,6 @@ import java.time.Duration;
 import java.util.Map;
 
 @Configuration
-@EnableCaching
 public class RedisCacheConfig {
 
     @Value("${app.cache-prefix:demo:}")
